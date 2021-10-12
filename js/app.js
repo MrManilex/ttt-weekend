c978c3dd6a218c394772182fc15af3244fa12b21
WangManLex <alexandermaniwang99@gmail.com>
2021-10-12 10:19:55 -0400

    adds cached elements

--- js/app.js
+++ js/app.js
@@ -12,9 +12,16 @@ let turn
 let winner
 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const boxOne = document.getElementById(sq0)
+const boxTwo = document.getElementById(sq1)
+const boxThree = document.getElementById(sq2)
+const boxFour = document.getElementById(sq3)
+const boxFive = document.getElementById(sq4)
+const boxSix = document.getElementById(sq5)
+const boxSeven = document.getElementById(sq6)
+const boxEight = document.getElementById(sq7)
+const boxNine = document.getElementById(sq8)
+const gameStatus = document.getElementById(message)
 /*----------------------------- Event Listeners -----------------------------*/
 
 
